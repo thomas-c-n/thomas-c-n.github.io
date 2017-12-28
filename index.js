4dac3e302afbdb117b89d066c6aad4b5e642cee7
thomas-c-n <thomas.c.narramore@gmail.com>
2017-12-28 15:06:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//ALARM CLOCK SETTINGS
+var goodMorning = 'goodMorning.mp3';
+var music = new Audio (goodMorning);
+
+
 
 //Date and time
 function time() {
@@ -28,16 +33,14 @@ var currentDateString = currentMonth +' '+ currentDate;
 var date = document.getElementById('date');
 date.innerHTML = currentDateString;
   
-  //ALARM CLOCK
-var musicSource = 'goodMorning.mp3';
-var music = new Audio (musicSource);
-if (currentHours === 2 && currentMinutes === 55 && currentSeconds === 0){
+  //ALARM CLOCK EXECUTION
+if (currentHours === 3 && currentMinutes === 5 && currentSeconds === 0){
   music.play();
 }
   
   
 }
-setInterval(time, 1000); //call the function every second
+setInterval(time, 500); //call the function every half second
 time();
 
 //Automatically refreshing the page every day
